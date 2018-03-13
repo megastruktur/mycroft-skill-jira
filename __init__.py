cfc8e5662f408befa5a732fb845617c9f58974b6
Peter Varaksin <p.varaksin@intetics.com>
2018-03-14 00:59:23 +0300

    Add some logic. Still WIP.

--- __init__.py
+++ __init__.py
@@ -16,25 +16,38 @@ class JiraSkill(MycroftSkill):
 
     def initialize(self):
 
-        jira_intent = IntentBuilder("JiraKeywordIntent"). \
-            require("TestKeyword").build()
-        self.register_intent(jira_intent,
-                             self.handle_jira_intent)
+        jira_intent = IntentBuilder("JiraIntent"). \
+            require("JiraKeyword").build()
+        self.register_intent(jira_intent, self.handle_jira_intent)
 
     def handle_jira_intent(self, message):
-        jira_server = 'https://jira.atlassian.net'
-        jira_user = '';
-        jira_password = '';
+        
+        # Load data from mycroft.conf
+        # JiraSkill prefix.
+        jira_server = self.config.get('jira_server')
+        jira_user = self.config.get('jira_user')
+        jira_password = self.config.get('jira_password')
+        #jira_project = self.config.get('jira_project')
+        jira_project = "SEN";
+        
+        print jira_server
+        print jira_user
+        print jira_password
+        
         jira_options = {'server': jira_server}
         
         authed_jira = JIRA(options=jira_options, basic_auth=(jira_user, jira_password))
-        issues = authed_jira.search_issues('assignee=""')
-        projects = authed_jira.projects()
-        for v in projects:
-           print v
+        issues = authed_jira.search_issues('assignee="' + jira_user + '" and project=' + jira_project, maxResults=3)
         
-        #self.speak_dialog("test")
-        self.speak("test")
+        for issue in issues:
+          self.speak(issue.fields.summary)
+          print issue.fields.summary
+          
+        #projects = authed_jira.projects()
+        #for v in projects:
+        #   print v
+        
+        self.speak_dialog("jira")
 
     def stop(self):
         pass
